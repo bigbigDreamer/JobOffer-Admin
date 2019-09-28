b05f5285c09f77c2e4a93c7718e942a88c13d9ad
bigbigDreamer <1826001146@qq.com>
2019-09-28 21:36:36 +0800

    完善商家管理查找、修改功能

--- src/apis/business/index.js
+++ src/apis/business/index.js
@@ -2,4 +2,6 @@ import  {request} from "../../tools"
 
 // 查询所有
 
-export const findAll = (config) => request.get("Business/findAll",config);
+export const findAll = config => request.get("Business/findAll",config);
+
+export const deleteById = (data,config) => request.post("Business/deleteById",data,config);
--- src/components/business/index.jsx
+++ src/components/business/index.jsx
@@ -1,9 +1,10 @@
 import React, {Component} from "react"
-import {Tabs, Select, Table, Button} from 'antd';
+import {Tabs, Select, Table, Button, Modal, Divider, message} from 'antd';
 import "./business.less"
-import {findAll} from "../../apis/business"
+import {findAll, deleteById} from "../../apis/business"
 
 const {Option} = Select;
+const {confirm} = Modal;
 const provinceData = ['甘肃省', '江苏省'];
 const cityData = {
     '甘肃省': ['兰州市', '张掖市', '武威市'],
@@ -20,6 +21,7 @@ export default class Business extends Component {
     state = {
         cities: cityData[provinceData[0]],
         secondCity: cityData[provinceData[0]][0],
+        // 表头
         columns: [
             {
                 title: '企业名称',
@@ -54,9 +56,9 @@ export default class Business extends Component {
             {
                 title: '详情',
                 key: 'detail',
-                render: _ => {
+                render: (text, record) => {
                     return (
-                        <Button>详情</Button>
+                        <Button onClick={() => this.showModal(record)}>详情</Button>
                     )
                 }
             },
@@ -68,12 +70,13 @@ export default class Business extends Component {
                         <div>
                             <Button type={"primary"} size={"small"}>修改</Button>
                             &nbsp;
-                            <Button type={"danger"} size={"small"}>删除</Button>
+                            <Button type={"danger"} size={"small"} onClick={() => this.handleDel(record)}>删除</Button>
                         </div>
                     )
                 }
             },
         ],
+        // 表格数据
         data: [
             {
                 "id": 2355,
@@ -92,9 +95,15 @@ export default class Business extends Component {
                 "city": null
             },
         ],
-        loading: true
+        // 表格加载
+        loading: true,
+        // 模态框
+        visible: false,
+        // 模态框数据
+        modalData: {}
     };
 
+    // 改变省份
     handleProvinceChange = value => {
         this.setState({
             cities: cityData[value],
@@ -102,17 +111,88 @@ export default class Business extends Component {
         });
     };
 
+    // 改变城市
     onSecondCityChange = value => {
         this.setState({
             secondCity: value,
         });
     };
 
+
     handleChange = () => {
 
     };
 
+    // 删除数据
+    handleDel = (data) => {
+
+        confirm({
+            title: '你确定删除当前数据吗?',
+            content: '如果不确定可以点击取消',
+            okText: '删除',
+            okType: 'danger',
+            cancelText: '取消',
+            onOk: () => {
+                deleteById({
+                    id: data.id
+                })
+                    .then(data => {
+                        // console.log(data, "删除");
+                        if (data.status === 200) {
+                            message.success('删除成功！');
+                            findAll()
+                                .then(data => {
+                                    if (data) {
+                                        this.setState({
+                                            data: data.data,
+                                            loading: false
+                                        })
+                                    }
+
+                                })
+                        } else {
+
+                        }
+                    })
+                    .catch(err => Promise.reject(err));
+            },
+            onCancel() {
+                console.log('Cancel');
+            },
+        });
+    };
+
+
+    // 模态框事件
+    showModal = (data) => {
+        // console.log(data);
+
+        this.setState({
+            visible: true,
+            modalData: data
+        });
+    };
+
+    // 点击确定
+    handleOk = e => {
+        // console.log(e);
+        this.setState({
+            visible: false,
+        });
+    };
+
+    // 点击取消
+    handleCancel = e => {
+        // console.log(e);
+        this.setState({
+            visible: false,
+        });
+    };
+
+    // 组件挂在完毕
     componentDidMount() {
+
+        // 查询所有数据
         findAll()
             .then(data => {
                 if (data) {
@@ -126,7 +206,9 @@ export default class Business extends Component {
     }
 
     render() {
-        const {cities, columns, data, loading} = this.state;
+        const {cities, columns, data, loading, visible, modalData} = this.state;
+
+        // 选中行
         const rowSelection = {
             onChange: (selectedRowKeys, selectedRows) => {
                 console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
@@ -200,12 +282,50 @@ export default class Business extends Component {
                                 <Option value="Yiminghe">yiminghe</Option>
                             </Select>
                         </div>
+
+                        {/*数据表*/}
                         <div className="table">
                             <Table
                                 rowSelection={rowSelection}
                                 columns={columns}
                                 loading={loading}
                                 dataSource={data}/>
+                            {/*详细信息模态框*/}
+                            <Modal
+                                title={modalData.name}
+                                visible={visible}
+                                onOk={this.handleOk}
+                                className={"modal"}
+                                onCancel={this.handleCancel}
+                                okText={"确定"}
+                                cancelText={"取消"}
+                            >
+                                <div className="top" style={{
+                                    margin: 0,
+                                    padding: 0,
+                                    fontSize: 12,
+                                }}>
+                                    <div>{modalData.industry}</div>
+                                    <div>{modalData.scale}</div>
+                                </div>
+                                <Divider/>
+                                <div className="content">
+                                    <p>行业类型: {modalData.industry}</p>
+                                    <p>成立时间: {modalData.establishedTime}</p>
+                                    <p>注册资本: {modalData.registeredCapital}</p>
+                                </div>
+                                <Divider/>
+                                <div className="description"
+                                     style={{
+                                         margin: 0,
+                                         padding: 0,
+                                         fontSize: 12,
+                                     }}>
+                                    <p>{
+                                        modalData.description
+                                    }</p>
+                                </div>
+                            </Modal>
                         </div>
                     </TabPane>
                 </Tabs>,
